94b905f0cc5f73dff5d623bd2fdaa0f498518862
Andre Perez <andreperezgarza@hotmail.com>
2023-01-26 23:04:45 -0600

    Added Docker file

---
+++ API-Project-delivery/src/test/java/org/AndrePerezGarza/API/Project/delivery/Controller/MovieControllerTest.java
@@ -0,0 +1,40 @@
+package org.AndrePerezGarza.API.Project.delivery.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findId() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getLink() {
+    }
+
+    @Test
+    void getMovie() {
+    }
+
+    @Test
+    void saveMovie() {
+    }
+}
